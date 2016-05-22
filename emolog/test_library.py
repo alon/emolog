178ed34d9d2a21f6a478dcf2227e875ea39f6b6d
Alon Levy <alon@pobox.com>
2016-05-22 21:55:09 +0300

    emolog/test_library.py: use pytest.mark.parametrize, passes all tests

--- emolog/test_library.py
+++ emolog/test_library.py
@@ -1,27 +1,35 @@
 import unittest
 
+import pytest
+
 import emolog
 
 
-class TestEmolog(unittest.TestCase):
+test_decode_sanity_data = [
+    ('version', emolog.Version, {}),
+    ('ping', emolog.Ping, {}),
+    ('ack', emolog.Ack, {"reply_to_seq": 5}),
+    ('sampler_register_variable', emolog.SamplerRegisterVariable,
+     {
+        'phase_ticks': 1,
+        'period_ticks': 2,
+        'address': 128,
+        'size': 4
+     }),
+    ('sampler_clear', emolog.SamplerClear, {}),
+    ('sampler_start', emolog.SamplerStart, {}),
+    ('sampler_stop', emolog.SamplerStop, {}),
+    ('sampler_sample', emolog.SamplerSample,
+     {
+         "ticks": 256,
+         "var_size_pairs": [(512, 2), (1024, 4)]
+     }),
+]
 
-    def test_decode_sane(self):
-        parser = emolog.ClientParser()
-        for name, reply_class, params in [
-            ('version', emolog.Version, {}),
-            ('ping', emolog.Ping, {}),
-            ('ack', emolog.Ack, {"reply_to_seq": 5}),
-            ('sampler_start', emolog.SamplerStart, {}),
-            ('sampler_stop', emolog.SamplerStop, {}),
-            ('sampler_clear', emolog.SamplerClear, {}),
-            ('sampler_register_variable', emolog.SamplerRegisterVariable,
-             {}),
-            ('sampler_sample', emolog.SamplerSample,
-             {
-                 "ticks": 4242,
-                 "var_size_pairs": [(1234, 2), (5678, 4)]
-             }),
-            ]:
-            encoded = getattr(emolog, 'encode_{}'.format(name))(**params)
-            msg = parser.incoming(encoded)
-            self.assertIsInstance(msg, reply_class)
+
+@pytest.mark.parametrize("name,reply_class,params", test_decode_sanity_data)
+def test_decode_sane(name, reply_class, params):
+    encoded = getattr(emolog, 'encode_{}'.format(name))(**params)
+    parser = emolog.ClientParser()
+    msg = parser.incoming(encoded)
+    assert isinstance(msg, reply_class), "expected {}, got {}".format(reply_class, str(msg))
