d54bea52cd187034970a4d09a4087890f221a17f
tinkerguy <guyovadia1@gmail.com>
2016-05-22 23:55:26 +0300

    send_version_to_client: windows support (generic serial opening)

--- emolog/send_version_to_client.py
+++ emolog/send_version_to_client.py
@@ -2,15 +2,30 @@
 
 import ctypes
 import struct
+import argparse
+import sys
 
 from serial import Serial
+from serial.tools.list_ports import comports
 
 from emolog import write_version, ClientParser, Version, SkipBytes, get_seq
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--serial', default=None, help='serial port to connect to (platform specific)')
+    args = parser.parse_args()
+    if args.serial is None:
+        stellaris = [x for x in comports() if 'stellaris' in x.description.lower()]
+        if len(stellaris) == 0:
+            print("no stellaris com port found - is the board connected and powered?")
+            raise SystemExit
+        if len(stellaris) > 1:
+            print("picking the first out of available {}".format(','.join([x.device for x in stellaris])))
+        args.serial = stellaris[0].device
     print("library seq: {}".format(get_seq()))
-    s = Serial('/dev/ttyACM0', baudrate=115200)
+    print("opening port {}".format(args.serial))
+    s = Serial(args.serial, baudrate=115200)
     success = 0
     write_version(s)
     parser = ClientParser()
