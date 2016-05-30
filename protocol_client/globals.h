ce8c995ab65d7d864f32c99a9f8bafc7964531aa
tinkerguy <guyovadia1@gmail.com>
2016-05-30 22:37:49 +0300

    add missing globals.h

---
+++ protocol_client/globals.h
@@ -0,0 +1,16 @@
+/*
+ * globals.h
+ *
+ *  Created on: 29 במאי 2016
+ *      Author: Guy Ovadia
+ */
+
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+
+
+extern uint32_t g_ui32SysClock;
+
+
+
+#endif /* GLOBALS_H_ */
