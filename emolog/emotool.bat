6eefadad4b37f7418da62df407901f98240a0a9b
tinkerguy <guyovadia1@gmail.com>
2016-10-31 20:56:19 +0200

    touch DLL so emolog.py doesn't try making it

--- emolog/emotool.bat
+++ emolog/emotool.bat
@@ -1,1 +1,2 @@
+copy /b emolog.dll +,,
 python emotool.py --serial COM34 --elf ..\protocol_embedded\debug\protocol_embedded.out --var sine,float,1,0 --var sawtooth,int,1,0 --verbose >> out.log  2>&1
