73c85b8503169b76cc31ccf8f3f5666041dd3be2
tinkerguy <guyovadia1@gmail.com>
2016-11-29 19:04:05 +0200

    remove old batch files

--- emolog/emotool_no_capture.bat
+++
@@ -1,2 +0,0 @@
-copy /b emolog.dll +,,
-python emotool.py --serial COM34 --elf ..\protocol_embedded\debug\protocol_embedded.out --var sine,1,0 --var sawtooth,1,0
--- emolog/emotool_start.bat
+++
@@ -1,2 +0,0 @@
-copy /b emolog.dll +,,
-python emotool.py --serial COM45 --baud 6000000 --elf ..\..\pump_drive_tiva\debug\pump_drive_tiva.out --varfile vars.csv --runtime 0.01 --silent --no-cleanup
