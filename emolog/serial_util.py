8fb816d93902fe48d137d83ff8b9ef61ad50c889
Alon Levy <alon@pobox.com>
2016-12-01 19:46:54 +0200

    serial2tcp: implement automatic comport detection, make it default; use argparse

---
+++ emolog/serial_util.py
@@ -0,0 +1,22 @@
+from serial.tools.list_ports import comports
+
+SERIAL_AUTO_VENDOR_ID = 0x0403
+SERIAL_AUTO_PRODUCT_ID = 0x6010
+
+def find_serial():
+    available = comports()
+    if len(available) == 0:
+        print("no com ports available - is board powered and connected?")
+        raise SystemExit
+    available = [ser for ser in available if ser.vid == SERIAL_AUTO_VENDOR_ID and ser.pid == SERIAL_AUTO_PRODUCT_ID]
+    if len(available) == 0:
+        print("no com port matching vendor/product ids available - is board powered and connected?")
+        raise SystemExit
+    if len(available) > 1:
+        # pick the lowest interface in multiple interface devices
+        device = min([(x.device_path.split('/')[:-1], x) for x in available])[1]
+    else:
+        device = available[0]
+    comport = device.device
+    print("automatic comport selection: {}".format(comport))
+    return comport
