ec477525b4183578d1c7ee74127275d2d99aa71a
Alon Levy @NaomLaptop <alon@pobox.com>
2017-06-27 18:10:22 +0300

    remove profile.bat

--- emolog/profile.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-copy /b emolog.dll +,, > nul
-python -mcProfile emotool.py --baud 8000000 --elf ..\..\pump_drive\Release\pump_drive_tiva.out --varfile vars.csv --runtime 0.5 %1 %2 %3 %4 %5 %6
