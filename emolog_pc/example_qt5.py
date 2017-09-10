78e577633bc023fa4872d98b558cbb5256cf59cf
Alon Levy <alon@pobox.com>
2017-09-11 02:14:16 +0300

    wip pyqtgraph - don't understand why I get a second widget, and how I fucked the x & y generation - need to redo that in a separate function and test it

--- emolog_pc/example_qt5.py
+++ emolog_pc/example_qt5.py
@@ -12,69 +12,33 @@
 from __future__ import unicode_literals
 import sys
 import os
-import random
 from datetime import datetime
-
-#from matplotlib.backends import qt_compat
 
 from Qt import QtGui, QtCore, QtWidgets
 
-from numpy import arange, sin, pi
-from matplotlib.backends.backend_qt5agg import FigureCanvas
-from matplotlib.figure import Figure
+from numpy import sin
+import pyqtgraph as pg
 
 progname = os.path.basename(sys.argv[0])
 progversion = "0.1"
-
-
-class MyMplCanvas(FigureCanvas):
-    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-
-        self.compute_initial_figure()
-
-        #
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-
-        FigureCanvas.setSizePolicy(self,
-                                   QtWidgets.QSizePolicy.Expanding,
-                                   QtWidgets.QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
-
-    def compute_initial_figure(self):
-        pass
-
-
-class MyStaticMplCanvas(MyMplCanvas):
-    """Simple canvas with a sine plot."""
-
-    def compute_initial_figure(self):
-        t = arange(0.0, 3.0, 0.01)
-        s = sin(2*pi*t)
-        self.axes.clear()
-        self.axes.plot(t, s)
 
 
 class ValuesGenerator:
     def callback(self, t):
         st = sin(t * 100)
         st2 = st * st
-        return [4 + st, 2 + st + st2, 4 - st, 2 - st2 + st]
+        return 4 + st, 2 + st + st2, 4 - st, 2 - st2 + st
+
 
 vg = ValuesGenerator()
 callback = vg.callback
 
-class MyDynamicMplCanvas(MyMplCanvas):
+
+class MyPlotWindow(pg.PlotWidget):
     """A canvas that updates itself every second with a new plot."""
 
-    callback = None
-
-    def __init__(self, *args, **kwargs):
-        MyMplCanvas.__init__(self, *args, **kwargs)
+    def __init__(self, parent):
+        pg.PlotWindow.__init__(self, parent=parent, title="my plot widget")
         timer = QtCore.QTimer(self)
         self.t = []
         self.vals = []
@@ -82,34 +46,36 @@ class MyDynamicMplCanvas(MyMplCanvas):
         timer.timeout.connect(self.update_figure)
         timer.start(200)
 
-    def compute_initial_figure(self):
-        self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], 'r')
-
     def update_figure(self):
         now = datetime.now()
         dt = (now - self.start).total_seconds()
-        t = self.t[-1] if len(self.t) > 0 else 0
-        #print(f'{len(self.t)}, {len(}')
-        for i in range(200):
-            self.call_callback(t + dt / 200 * i)
+        t0 = self.t[-1] if len(self.t) > 0 else 0
+        t = [t0 + float(i) * 200 / dt for i in range(200)]
+        self.call_callback(t)
         self.redraw()
 
     def call_callback(self, t):
         # Build a list of 4 random integers between 0 and 10 (both inclusive)
 
-        self.t = (self.t + [t])[-5000:]
+        self.t = (self.t + t)[-5000:]
         new_vals = callback(t)
         #new_vals = [1, 1.2]
         if len(self.vals) == 0:
-            self.vals = [[x] for x in new_vals]
+            self.vals = [x for x in new_vals]
         else:
-            self.vals = [(vs + [new_x])[-5000:] for vs, new_x in zip(self.vals, new_vals)]
+            print(f'len(self.vals) = {len(self.vals)}')
+            print(f'len(new_vals) = {len(new_vals)}')
+            cur_vals = self.vals
+            del self.vals[:]
+            for vs, new_x in zip(cur_vals, new_vals):
+                print(f'len(vs) = {len(vs)}')
+                print(f'len(new_x = {len(new_x)}')
+                self.vals.append((vs + new_x)[-5000:])
 
     def redraw(self):
         args = sum([[self.t, l] for l in self.vals], [])
-        self.axes.plot(*args)
-        self.axes.set_ylim([-2, 10])
-        self.draw()
+        self.plot(*args, clear=True)
+        #self.axes.set_ylim([-2, 10])
 
 
 class ApplicationWindow(QtWidgets.QMainWindow):
@@ -132,13 +98,13 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.main_widget = QtWidgets.QWidget(self)
 
         l = QtWidgets.QVBoxLayout(self.main_widget)
-        dc = MyDynamicMplCanvas(self.main_widget, width=5, height=4, dpi=100)
-        l.addWidget(dc)
+        plot_widget = MyPlotWindow(self.main_widget)
+        l.addWidget(plot_widget)
 
         self.main_widget.setFocus()
         self.setCentralWidget(self.main_widget)
 
-        self.statusBar().showMessage("All hail matplotlib!", 2000)
+        self.statusBar().showMessage("All hail streamplot!", 2000)
 
     def fileQuit(self):
         self.close()
