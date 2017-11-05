85340d9ac75e618b40418f67d42170f1c0e474bf
Alon Levy <alon@pobox.com>
2017-11-05 15:05:05 +0200

    cython: no longer need emolog_protocol library (wrapped in cylib.pyx)

--- emolog_pc/emolog/setup.py
+++ emolog_pc/emolog/setup.py
@@ -51,10 +51,6 @@ def build_artifacts():
     Builds or copies any file that isn't already in emolog directory into it,
     and returns a list of those files relative to the emolog directory.
     """
-    print(f"building {LIB_FILENAME}")
-    protocol_lib_path = build_protocol_library()
-    print(f"copying {LIB_FILENAME}")
-    copy(protocol_lib_path, 'emolog')
     MESSAGE_TYPE_H_FILENAME = path.basename(DEVEL_EMO_MESSAGE_TYPE_H_FILENAME)
     print(f"copying {MESSAGE_TYPE_H_FILENAME}")
     copy(DEVEL_EMO_MESSAGE_TYPE_H_FILENAME, 'emolog')
