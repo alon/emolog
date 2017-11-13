0b3bc94ea17cd9b6d3706fd68823e8bc8d28ef14
Alon Levy <alon@pobox.com>
2017-11-13 21:25:50 +0200

    emolog/serial2tcp: remove serial_util (fold into serial2tcp)
    It was only being used by serial2tcp anyway, contained one function,
    and the existing import to it was broken - using a relative import
    but run not in a package context.

--- emolog_pc/emolog/emotool/serial2tcp.py
+++ emolog_pc/emolog/emotool/serial2tcp.py
@@ -11,9 +11,33 @@ import logging
 import signal
 from argparse import ArgumentParser
 
+from serial.tools.list_ports import comports
 import colorama
 
-from .serial_util import find_serial
+
+SERIAL_AUTO_VENDOR_ID = 0x0403
+SERIAL_AUTO_PRODUCT_ID = 0x6010
+
+def find_serial():
+    available = comports()
+    if len(available) == 0:
+        print("no com ports available - is board powered and connected?")
+        raise SystemExit
+    available = [ser for ser in available if ser.vid == SERIAL_AUTO_VENDOR_ID and ser.pid == SERIAL_AUTO_PRODUCT_ID]
+    if len(available) == 0:
+        print("no com port matching vendor/product ids available - is board powered and connected?")
+        raise SystemExit
+    if len(available) > 1:
+        # pick the lowest interface in multiple interface devices
+        if hasattr(available[0], 'device_path'):
+            device = min([(x.device_path.split('/')[:-1], x) for x in available])[1]
+        else:
+            device = min([(x.device, x) for x in available])[1]
+    else:
+        device = available[0]
+    comport = device.device
+    print("automatic comport selection: {}".format(comport))
+    return comport
 
 
 log = logging.getLogger('serial2tcp')
