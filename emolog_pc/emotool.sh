2dc51409b67293093c54dd9c6356d55aeb7a3967
Alon Levy <alon@pobox.com>
2017-11-14 09:48:17 +0200

    emotool.{bat,sh}: add sh and fix bat for location of emotool.py

---
+++ emolog_pc/emotool.sh
@@ -0,0 +1,2 @@
+@echo off
+python emotool.py --baud 8000000 --hw_flow_control --elf ../../pump_drive/Release/pump_drive_tiva.out --snapshotfile snapshot_vars.csv --varfile vars.csv --runtime 3.0 --no_processing %1 %2 %3 %4 %5 %6
