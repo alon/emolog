0d37dc2762dcd259a4bf53d5c9df609c911f18f4
Alon Levy <alon@pobox.com>
2017-11-16 14:55:43 +0200

    appveyor: update to use prof.py

--- emolog_pc/misc/prof.py
+++ emolog_pc/misc/prof.py
@@ -1,2 +1,11 @@
+import pstats, cProfile
+import sys
+
 from emolog.emotool.main import main
-main()
+
+sys.argv = ['prof.py', '--fake', '--runtime', '10']
+
+cProfile.runctx("main()", globals(), locals(), "prof.prof")
+
+s = pstats.Stats("prof.prof")
+s.strip_dirs().sort_stats("time").print_stats(40)
