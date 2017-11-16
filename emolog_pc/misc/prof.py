367d18eb5cc76123b8bb396d033dbd82b9eb3fba
Alon Levy <alon@pobox.com>
2017-11-16 15:26:19 +0200

    prof.py: tweak

--- emolog_pc/misc/prof.py
+++ emolog_pc/misc/prof.py
@@ -1,11 +1,12 @@
 import pstats, cProfile
 import sys
+import os
 
 from emolog.emotool.main import main
 
-sys.argv = ['prof.py', '--fake', '--runtime', '10']
+sys.argv = ['python', os.path.join('pyinstaller', 'emotool.py'), '--fake', '--runtime', '10']
 
 cProfile.runctx("main()", globals(), locals(), "prof.prof")
 
 s = pstats.Stats("prof.prof")
-s.strip_dirs().sort_stats("time").print_stats(40)
+s.strip_dirs().sort_stats("cumtime").print_stats(40)
