74fc4742bd524cf3d98bfa2bedfdf545af769df4
Alon Levy <alon@pobox.com>
2017-11-24 07:30:55 +0200

    emolog_example_client: add c++ support by setting linkage to C

--- examples/examples_common/emolog_example_client.h
+++ examples/examples_common/emolog_example_client.h
@@ -10,9 +10,16 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void emolog_example_main_loop(void);
 void delay_ms(uint32_t ms);     // this needs to be implemented externally, as the example code uses it
 
+#ifdef __cplusplus
+}
+#endif
 
 
 #endif /* EMOLOG_EXAMPLE_CLIENT_H_ */
