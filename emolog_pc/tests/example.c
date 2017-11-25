2ed08f17fb53ed245e40c1c701801bd35c27332f
Alon Levy <alon@pobox.com>
2017-11-25 17:38:22 +0200

    test update

--- emolog_pc/tests/example.c
+++ emolog_pc/tests/example.c
@@ -1,9 +1,17 @@
-int xthing;
+int var_int = 10;
+float var_float = 20.0;
+unsigned char var_unsigned_char = 40;
+float var_float8[8] = {0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005};
+const float var_float_arr_2[] [2] = {
+    { 0.1, 0.2 },
+    { 0.2, 0.3 },
+    { 0.3, 0.4 },
+    { 1.2, 1.3 },
+    { 1.3, 1.4 },
+};
 
 int main(void)
 {
- xthing = 0;
- while (xthing < 1000);
-  xthing++;
- return xthing;
+    float bla = (1000.0) * (var_float8[3] + var_float8[2] + var_float8[1] + var_float8[0] + var_float_arr_2[0][0]);
+    return var_int + var_float + var_unsigned_char + bla;
 }
