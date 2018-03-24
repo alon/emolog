f4d1dc8fb9b1c5c7052b6a556c179644545d6967
Alon Levy <alon@pobox.com>
2018-03-24 23:18:30 +0300

    emotool.sh: use setup.py installed console tool

--- emolog_pc/emotool.sh
+++ emolog_pc/emotool.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python emotool.py --baud 8000000 --hw_flow_control --elf ../../pump_drive/Release/pump_drive_tiva.out --snapshotfile snapshot_vars.csv --varfile vars.csv --runtime 3.0 --no_processing $@
+emotool --baud 8000000 --hw_flow_control --elf ../../pump_drive/Release/pump_drive_tiva.out --snapshotfile snapshot_vars.csv --varfile vars.csv --runtime 3.0 --no_processing $@
