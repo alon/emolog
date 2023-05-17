63c63583bcf7c2a0dd55ddd0b44f049aeb08396d
tinkerer42 <guy@orcontech.com>
2023-05-18 00:20:29 +0300

    bundle the post processor

--- emolog_pc/pyinstaller/bundle.bat
+++ emolog_pc/pyinstaller/bundle.bat
@@ -1,4 +1,5 @@
 pyinstaller emotool.spec --noconfirm
+pyinstaller "..\..\..\emolog_work\post_processor.py"
 copy "..\..\..\emolog_work\for pyInstaller\emolog.bat" dist
 copy "..\..\..\emolog_work\for pyInstaller\local_machine_config.ini" dist
 md dist\Outputs
