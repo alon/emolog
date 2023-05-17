5cffaad2cba8d33549d622177146bc51a7e86bf3
tinkerer42 <guy@orcontech.com>
2023-05-18 01:46:06 +0300

    bundle.bat: remove temp copy of ELF

--- emolog_pc/pyinstaller/bundle.bat
+++ emolog_pc/pyinstaller/bundle.bat
@@ -3,7 +3,3 @@ pyinstaller "..\..\..\emolog_work\post_processor.py"
 copy "..\..\..\emolog_work\for pyInstaller\emolog.bat" dist
 copy "..\..\..\emolog_work\for pyInstaller\local_machine_config.ini" dist
 md dist\Outputs
-
-:: TEMP
-copy "..\..\..\Debug\BQ2.0.elf" dist
-
