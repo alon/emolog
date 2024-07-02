87a06ac1e5c54c140f708cb09dcb8a017f98fae2
tinkerer42 <guy@orcontech.com>
2024-07-02 15:45:49 +0300

    update files for pyInstaller
    new pyinstaller puts stuff in _internal folder and this required change in how some files are bundled

--- emolog_pc/pyinstaller/bundle.bat
+++ emolog_pc/pyinstaller/bundle.bat
@@ -2,4 +2,6 @@ pyinstaller emotool.spec --noconfirm
 pyinstaller "..\..\..\emolog_work\post_processor.py"
 copy "..\..\..\emolog_work\for pyInstaller\emolog.bat" dist
 copy "..\..\..\emolog_work\for pyInstaller\local_machine_config.ini" dist
+copy "..\..\..\emolog_work\vars.csv" dist\emotool
+copy "..\..\..\emolog_work\snapshot_vars.csv" dist\emotool
 md dist\Outputs
