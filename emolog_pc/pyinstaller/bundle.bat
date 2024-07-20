33c00549a7ea1eed3b478d3b6be20fd59833cf7a
tinkerer42 <guy@orcontech.com>
2024-07-20 17:21:05 +0300

    bundle.bat: minor changes
    to conform to same format as in NN project

--- emolog_pc/pyinstaller/bundle.bat
+++ emolog_pc/pyinstaller/bundle.bat
@@ -1,7 +1,7 @@
 pyinstaller emotool.spec --noconfirm
-pyinstaller "..\..\..\emolog_work\post_processor.py"
+pyinstaller "..\..\..\emolog_work\post_processor.py" --noconfirm
+copy "..\..\..\emolog_work\vars.csv" dist\emotool
 copy "..\..\..\emolog_work\for pyInstaller\emolog.bat" dist
 copy "..\..\..\emolog_work\for pyInstaller\local_machine_config.ini" dist
-copy "..\..\..\emolog_work\vars.csv" dist\emotool
 copy "..\..\..\emolog_work\snapshot_vars.csv" dist\emotool
 md dist\Outputs
