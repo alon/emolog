4c0de4629a17b15c0e553cb79f43fdc28123c7f6
tinkerer42 <guy@orcontech.com>
2026-05-18 14:40:18 +0300

    remove some very old irrelevant stuff

--- emolog_pc/copy_to_alon.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-scp ../../pump_drive_tiva/Debug/pump_drive_tiva.out vars.csv alon@blues.local:
--- emolog_pc/emotool.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-emotool --baud 8000000 --hw_flow_control --elf ../../pump_drive/Release/pump_drive_tiva.out --snapshotfile snapshot_vars.csv --varfile vars.csv --runtime 3.0 --no_processing $@
